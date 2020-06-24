a01ea310894f717926b053d467a9e23ec11174eb
hazevt04 <hazelnutvt04@gmail.com>
2020-06-24 00:17:57 -0400

    Updated instantiations for the vectors

--- src/try_thrust_man_vec.cu
+++ src/try_thrust_man_vec.cu
@@ -1,6 +1,8 @@
 #include <cuda_runtime.h>
-
+#include <cufft.h>
 #include "managed_allocator.h"
+#include <thrust/generate.h>
+#include <thrust/transform.h>
 
 template<class T>
 using managed_device_vector = thrust::device_vector<T, managed_allocator<T, cudaMemAttachGlobal>>;
@@ -13,11 +15,8 @@ int main( int argc, char** argv) {
    int num_vals = 1000;
    int window_size = 40;
    int num_sums = num_vals - window_size;
-   managed_device_vector<float2> mdv;
-   managed_host_vector<float2> mhv;
-
-   mdv.reserve( num_vals );
-   mhv.reserve( num_sums );
+   managed_device_vector<float2> mdv( num_vals );
+   managed_host_vector<float2> mhv( num_sums );
 
    return 0;
 }
