e33474346c8a320857b495263994a4152cf26627
Glenn Hazelwood <ghazelwood@ltsnet.net>
2020-06-24 11:23:59 -0400

    After a bit of a struggle on the Thrust learning curve,...finally got it to generate and print random cufftComplex values! (Yay.). Basically, don't assume thrust::device_vector and thrust::host_vectors are like std::vector. There's no thrust::host_vector::at(index), for example. I have to use [index]

--- src/try_thrust_man_vec.cu
+++ src/try_thrust_man_vec.cu
@@ -3,20 +3,50 @@
 #include "managed_allocator.h"
 #include <thrust/generate.h>
 #include <thrust/transform.h>
+#include <thrust/for_each.h>
+#include <algorithm>
+#include <random>
 
 template<class T>
 using managed_device_vector = thrust::device_vector<T, managed_allocator<T, cudaMemAttachGlobal>>;
 
+
 template<class T>
 using managed_host_vector = thrust::device_vector<T, managed_allocator<T, cudaMemAttachHost>>;
+
+
+cufftComplex gen_rand_cufftComplex() {
+   static std::default_random_engine r_engine;
+   static std::uniform_real_distribution<float> udist{0.0, 50.0}; // range 0 - 50
+
+   /*cufftComplex result{(float)udist(r_engine),(float)udist(r_engine)};*/
+   return cufftComplex{(float)udist(r_engine),(float)udist(r_engine)};
+}
+
+
+inline void cout_cufftComplex( const cufftComplex& val ) {
+   std::cout << "{ " << val.x << ", " << val.y << " }\n";
+}
+
 
 int main( int argc, char** argv) {
 
    int num_vals = 1000;
    int window_size = 40;
    int num_sums = num_vals - window_size;
-   managed_device_vector<float2> mdv( num_vals );
-   managed_host_vector<float2> mhv( num_sums );
+   
+   managed_device_vector<cufftComplex> vals( num_vals );
+   managed_device_vector<cufftComplex> sums( num_sums );
+
+   for( size_t index = 0; index != vals.size(); ++index ) {
+      vals[index] = gen_rand_cufftComplex();
+   } 
+
+   std::cout << "Vals:\n"; 
+   for( size_t index = 0; index != vals.size(); ++index ) {
+      cout_cufftComplex( vals[index] );  
+   } 
+   std::cout << "\n";
 
    return 0;
 }
